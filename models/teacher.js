877917e28daad5a611e1f44088353f2a877ba139
sami el <s.elidrissidafali@elaraki.ac.ma>
2024-10-28 10:17:24 -0400

    c

--- models/teacher.js
+++ models/teacher.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
+
 const jwt = require("jsonwebtoken");
 
 const teacherSchema = new mongoose.Schema({
